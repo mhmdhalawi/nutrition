49344ed84c7339bc7bb1bde1340483888e4dd823
mhmdhalawi <mohammadhalawi93@gmail.com>
2024-11-17 13:03:21 +0200

    ✨ enhance checkbox handling and conditional rendering in Facts component

--- src/components/Facts/Fact.vue
+++ src/components/Facts/Fact.vue
@@ -21,30 +21,39 @@ const handleChecked = (event: Event) => {
 
 <template>
   <div class="border-b border-black py-0.5 last:border-none">
-    <input type="checkbox" id="toggle" class="hidden" @change="handleChecked" />
+    <input
+      type="checkbox"
+      :checked="show"
+      :id="`${serving.id}`"
+      class="hidden"
+      @change="handleChecked"
+    />
     <label
-      for="toggle"
-      class="cursor-pointer flex justify-between font-black"
+      :for="`${serving.id}`"
+      class="flex justify-between font-black cursor-pointer"
       :class="{
         'ps-4 font-normal': serving.indentations === 1,
         'ps-8 font-normal': serving.indentations === 2,
       }"
     >
-      <p>
-        {{ serving[key] }}
-        <span class="font-normal"
-          >{{ Math.round(serving.value) }} {{ serving.unit ? serving.unit.name : "g" }}</span
+      <template v-if="show">
+        <p>
+          {{ serving[key] }}
+          <span class="font-normal"
+            >{{ Math.round(serving.value) }} {{ serving.unit ? serving.unit.name : "g" }}</span
+          >
+        </p>
+        <span
+          class="font-black"
+          :class="{
+            'font-normal': serving.section !== 0,
+          }"
+          >{{ dailyValue ? Math.round(dailyValue) : Math.round(serving.daily_value) }}%</span
         >
-      </p>
-      <span
-        class="font-black"
-        :class="{
-          'font-normal': serving.section !== 0,
-        }"
-        >{{ dailyValue ? Math.round(dailyValue) : Math.round(serving.daily_value) }}%</span
-      >
+      </template>
+      <template v-else>
+        <p>&nbsp;</p>
+      </template>
     </label>
-
-    <template v-if="show"> </template>
   </div>
 </template>
